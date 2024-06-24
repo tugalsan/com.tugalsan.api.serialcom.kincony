375723e00e6012e8efaa02c4cff91d310e2fec1f
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-06-24 16:11:57 +0300

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,7 +9,7 @@ module com.tugalsan.api.serialcom.kincony {
     requires com.tugalsan.api.string;
     requires com.tugalsan.api.thread;
     requires com.tugalsan.api.serialcom;
-    requires com.tugalsan.api.runnable;
+    
     exports com.tugalsan.api.serialcom.kincony.server.KC868_A32_R1_2;
     exports com.tugalsan.api.serialcom.kincony.server.KC868_A32_R1_2.core;
 }
